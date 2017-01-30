a97bb63e6ff99ab3c9ea021318d9b6421eb28514
Jaswant Singh <jaswant.singh@io-media.com>
2017-01-30 14:57:02 +0530

    Issues fixes.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import _ from 'lodash';
 
 export default class App extends Component {
     render() {
         return(
-            <h1>Hello World!!!</h1>
+            <ul>
+                {
+                    _.map(this.props.items, (item) => {
+                        return this.props.item(item);
+                    })
+                }
+            </ul>
         );
     }
 }
